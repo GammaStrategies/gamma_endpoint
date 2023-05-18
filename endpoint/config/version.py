5fdd66bb7ff04c1b87879063315f8a5d6196cf1f
iospap <pau@nabbo.com>
2023-05-18 18:24:02 +0200

    fix temporary

--- endpoint/config/version.py
+++ endpoint/config/version.py
@@ -2,7 +2,7 @@ import git
 import os
 
 APP_VERSION = "0.0.1"
-GIT_BRANCH = git.Repo(os.getcwd()).active_branch.name
+GIT_BRANCH = ""  # git.Repo(os.getcwd()).active_branch.name
 
 
 def get_version_info():
