11743f8ef00eff09668014833dea20ea7c0901fc
iospap <pau@nabbo.com>
2023-05-18 18:31:36 +0200

    try now

--- endpoint/config/version.py
+++ endpoint/config/version.py
@@ -1,8 +1,8 @@
-# import git
+import git
 import os
 
 APP_VERSION = "0.0.1"
-GIT_BRANCH = ""  # git.Repo(os.getcwd()).active_branch.name
+GIT_BRANCH = git.Repo(os.getcwd()).active_branch.name
 
 
 def get_version_info():
