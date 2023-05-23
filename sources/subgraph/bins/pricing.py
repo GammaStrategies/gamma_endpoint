68289e5b59c6af0551e5841a15781f77169d1608
loc <l0c4t0r@gmail.com>
2023-05-23 07:20:23 +0000

    fix USDC pricing on polygon

--- sources/subgraph/bins/pricing.py
+++ sources/subgraph/bins/pricing.py
@@ -139,6 +139,8 @@ POOL_PATHS = {
         ],
     },
     Chain.POLYGON: {
+        # USDC
+        "0x2791bca1f2de4661ed88a30c99a7a9449aa84174": [],
         # WMATIC
         "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270": [
             (POOLS[Chain.POLYGON]["WMATIC_USDC"], 1)
