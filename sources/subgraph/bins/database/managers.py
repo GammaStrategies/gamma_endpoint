9d8a3a36d570d3259d42f4ce21a34fe6b2416281
iospap <pau@nabbo.com>
2023-05-27 16:16:01 +0200

    clean

--- sources/subgraph/bins/database/managers.py
+++ sources/subgraph/bins/database/managers.py
@@ -1736,9 +1736,6 @@ class db_allRewards2_external_manager(db_allRewards2_manager):
                         f" {chain}'s {protocol} has same rewarder address with same reward token "
                     )
         except IndexError as e:
-            logger.exception(
-                f" allrewars2 external error {chain}'s {protocol}    err:{e}"
-            )
             raise IndexError(
                 f""" {chain}'s {protocol} has no rewards data in database {f"for {current_timestamp} timestamp" if current_timestamp else ""}"""
             ) from e
