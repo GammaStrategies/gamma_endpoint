3f65ab623511fec194ba148631ca4ef4b0977af2
iospap <pau@nabbo.com>
2023-08-30 11:54:44 +0200

    add output models

--- sources/internal/bins/internal.py
+++ sources/internal/bins/internal.py
@@ -20,3 +20,34 @@ class InternalFeeReturnsOutput(BaseModel):
     daily: InternalFeeYield = InternalFeeYield()
     weekly: InternalFeeYield = InternalFeeYield()
     monthly: InternalFeeYield = InternalFeeYield()
+
+
+class InternalTokens(BaseModel):
+    token0: float = 0
+    token1: float = 0
+    usd: float = 0
+
+
+class InternalTimeframe(BaseModel):
+    ini: int = 0
+    end: int = 0
+
+
+class InternalGrossFeesOutput(BaseModel):
+    """Output model for internal gross fees qtty in a period"""
+
+    symbol: str
+    days_period: int
+
+    block: InternalTimeframe = InternalTimeframe()
+    timestamp: InternalTimeframe = InternalTimeframe()
+
+    deposits: InternalTokens = InternalTokens()
+    withdraws: InternalTokens = InternalTokens()
+
+    collectedFees: InternalTokens = InternalTokens()
+
+    protocolFee_0: int = 0
+    protocolFee_1: int = 0
+
+    calculatedGrossFees: InternalTokens = InternalTokens()
