a70334cf7c4c959480a68e4add6f2bd7ef1f1e95
loc <l0c4t0r@gmail.com>
2023-09-10 14:02:11 +0000

    sqrtprice

--- sources/subgraph/bins/hypervisor.py
+++ sources/subgraph/bins/hypervisor.py
@@ -59,6 +59,7 @@ class HypervisorData:
                         symbol
                         decimals
                     }
+                    sqrtPriceX96
                 }
             }
         }
