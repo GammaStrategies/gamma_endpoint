2bdfa6159b7e08b968ac1a078ccd2c629c53a891
iospap <pau@nabbo.com>
2023-11-06 13:33:00 +0100

    revenue_stats descriptions

--- sources/frontend/endpoint/routers.py
+++ sources/frontend/endpoint/routers.py
@@ -58,6 +58,13 @@ class frontend_router_builder_main(router_builder_baseTemplate):
     ) -> list[dict]:
         """Returns Gamma's fees aquired by hypervisors, calculated volume of swaps on those same hypervisors and their revenue (Gamma service fees).
 
+        * **total_revenue** are all tokens transfered to Gamma's fee accounts from hypervisors and other sources (line veNFT). USD token prices are from the date the transfer happened.
+        * **total_fees** are all fees aquired by the hypervisors.  USD token prices are from the date it happened but can contain some posterior prices (week).
+        * **total_volume** is calculated using **total_fees**.
+
+        ### Query parameters
+        * **chain** Chain to filter by.
+        * **protocol** Protocol to filter by.
         * **from_timestamp** Limit returned data from timestamp to now.
         * **yearly** group result by year.
 
