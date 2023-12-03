7df16c0f15cfd888fa9ced441a9d3838e02b3dc3
iospap <pau@nabbo.com>
2023-12-03 07:34:25 +0100

    address filter mod

--- sources/common/general/utils.py
+++ sources/common/general/utils.py
@@ -1,17 +1,27 @@
-def filter_addresses(addresses: list[str]) -> list[str] | None:
-    """Discard any non address str from a list of addresses.
+def filter_addresses(addresses: list[str] | str) -> list[str] | str | None:
+    """Discard any non address str from addresses.
 
     Args:
-        addresses (list[str]): list of addresses
+        addresses (list[str] or str): list of addresses or one address
 
     Returns:
-        list[str] | None: list of addresses or None
+        list[str] | str | None: addresses or None
     """
     # remove any non address str
     if addresses:
-        return [
-            h.lower()
-            for h in addresses
-            if h and h.startswith("0x") and len(h) == 42 and h[2:].isalnum()
-        ]
+        if isinstance(addresses, str):
+            return (
+                addresses.lower()
+                if addresses
+                and addresses.startswith("0x")
+                and len(addresses) == 42
+                and addresses[2:].isalnum()
+                else None
+            )
+        else:
+            return [
+                h.lower()
+                for h in addresses
+                if h and h.startswith("0x") and len(h) == 42 and h[2:].isalnum()
+            ]
     return None
