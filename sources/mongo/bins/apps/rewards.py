e56ad55b8ca28a76702df301e856d03effae2b72
iospap <pau@nabbo.com>
2024-04-06 00:25:25 +0200

    temporal  mfd rewrite

--- sources/mongo/bins/apps/rewards.py
+++ sources/mongo/bins/apps/rewards.py
@@ -1,5 +1,7 @@
+import asyncio
 import logging
 from sources.common.general.enums import Chain, Protocol
+from sources.common.xt_api.ramses import ramses_api_helper
 from ..helpers import local_database_helper
 
 
@@ -93,4 +95,65 @@ async def latest_multifeeDistributor(network: Chain, protocol: Protocol):
             "boostApr"
         ] += boostApr
 
-    return result
+    return await rewrite_mfd_with_api(result, network)
+
+
+async def rewrite_mfd_with_api(data: dict, chain: Chain) -> dict:
+
+    try:
+        ramses_api = ramses_api_helper(chain=chain)
+        if not ramses_api:
+            logging.getLogger(__name__).warning("Ramses API not available")
+            return data
+
+        tmp, hypervisors_static = await asyncio.gather(
+            ramses_api.data(),
+            local_database_helper(network=chain).get_items_from_database(
+                collection_name="static",
+                find={},
+                projection={"_id": 0, "address": 1, "symbol": 1, "pool": 1},
+            ),
+        )
+        hypervisors_static = {x["address"]: x for x in hypervisors_static}
+
+        for mfd_address, item in data.items():
+            for hype_address, hype_data in item["hypervisors"].items():
+                # reset totals
+                hype_data["apr"] = 0
+                hype_data["baseApr"] = 0
+                hype_data["boostApr"] = 0
+
+                for rewarder_address, rewarder_data in hype_data["rewarders"].items():
+                    try:
+                        # get ramses api data
+                        tmp_data = await ramses_api.get_pool_apr(
+                            pool=hypervisors_static[hype_address]["pool"]["address"],
+                            token=rewarder_data["rewardToken"],
+                        )
+                    except Exception as e:
+                        logging.getLogger(__name__).error(
+                            f"Error getting ramses api data: {e}"
+                        )
+                        continue
+
+                    #### modify rewarder data
+                    ####
+                    rewarder_data["rewardPerSecond"] = tmp_data["rewardsPerSecond"]
+                    rewarder_data["apr"] = tmp_data["apr"]
+                    rewarder_data["baseApr"] = tmp_data["apr"]
+                    rewarder_data["boostApr"] = 0
+                    rewarder_data["baseRewardPerSecond"] = tmp_data["rewardsPerSecond"]
+                    rewarder_data["boostRewardPerSecond"] = 0
+
+                    #### modify totals
+                    ####
+                    hype_data["stakedAmountUSD"] = tmp_data["totalValueLockedUSD"]
+                    # hype_data["stakedAmount"] =
+                    hype_data["apr"] += tmp_data["apr"]
+                    hype_data["baseApr"] += tmp_data["apr"]
+                    hype_data["boostApr"] += 0
+
+    except Exception as e:
+        logging.getLogger(__name__).error(f"Error rewriting mfd with api: {e}")
+
+    return data
