ee46154579631eddd60b3496c2a101de25ef7b24
iospap <pau@nabbo.com>
2024-06-20 12:36:22 +0200

    gunicorn logs

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,3 +1,8 @@
 bind = "0.0.0.0:8080"
 worker_class = "uvicorn.workers.UvicornWorker"
-workers = 3
+workers = 5
+loglevel = "info"
+accesslog = "/var/log/gunicorn/access_log_gamma_endpoint"
+acceslogformat = "%(h)s %(l)s %(u)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s"
+errorlog = "/var/log/gunicorn/error_gamma_endpoint"
+timeout = 65
